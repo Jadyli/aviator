160c12aa6eb70b2d2557fe200f5fdbc26c2ea36b
Jadyli <jady1257984872@gmail.com>
2018-02-11 18:04:03 +0800

    add '${variable}' support.

--- src/test/java/com/googlecode/aviator/SimpleELPerformanceTest.java
+++ src/test/java/com/googlecode/aviator/SimpleELPerformanceTest.java
@@ -9,23 +9,24 @@ public class SimpleELPerformanceTest extends TestCase {
   public void test_perf() throws Exception {
 
     Map<String, Object> ctx = new HashMap<String, Object>();
-    ctx.put("a", 3);
-    ctx.put("b", 4);
-    ctx.put("c", 5);
+    ctx.put("${abc}", 3);
+    ctx.put("def", 4);
+    ctx.put("hi", 5);
 
     // AviatorEvaluator.setTrace(true);
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 1; ++i) {
       perf(ctx);
     }
   }
 
   private void perf(Map<String, Object> ctx) {
-    Expression exp = AviatorEvaluator.compile("(a+b)*c");
     long startMillis = System.currentTimeMillis();
+    Expression exp = AviatorEvaluator.compile("(${abc} + def) * hi > 35 ? '哈哈哈' : '呵呵呵'");
 
-    final int COUNT = 1000 * 1000;
+    final int COUNT = 1;
     for (int i = 0; i < COUNT; ++i) {
-      exp.execute(ctx);
+      Object execute = exp.execute(ctx);
+      System.out.println(execute);
     }
 
     long millis = System.currentTimeMillis() - startMillis;
